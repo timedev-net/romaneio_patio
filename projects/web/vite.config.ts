66a7c2a360d55354457865f41c1b051b17279191
Daniel Frota <drfrota.adv@gmail.com>
2023-04-06 17:56:48 -0400

    att libs

--- projects/web/vite.config.ts
+++ projects/web/vite.config.ts
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {port: 3001},
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4000,
+    strictPort: true,
+  },
 })
